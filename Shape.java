36dec2994d6816c0a00f76be6ed7ee2cee8745fe
SEONMIN99 <seomin99@naver.com>
2019-09-02 13:59:22 +0900

    dd

---
+++ Shape.java
@@ -0,0 +1,19 @@
+
+/**
+ *   인터페이스 Shape - 여기에 인터페이스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전 번호나 날짜)
+ */
+
+public interface Shape
+{
+    final double PI = 3.14; // 상수
+    void draw(); // 도형을 그리는 추상 메소드
+    double getArea(); // 도형의 면적을 리턴하는 추상 메소드
+    default public void redraw(){ // 디폴트 메소드
+        System.out.print("--"); 
+        draw(); 
+    }
+}
+
