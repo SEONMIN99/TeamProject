1082f8c81e5a46ba7cce987e4edd4342496bf0a7
SEONMIN99 <seonmin99@naver.com>
2019-09-04 15:15:02 +0900

    타원

--- Oval.java
+++ Oval.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Oval
+public class Oval implements Shape
 {
     // instance variables - replace the example below with your own
     private int x;
