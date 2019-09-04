2d38ba3c48253013ce97db6054746e9548214d9e
SEONMIN99 <seonmin99@naver.com>
2019-09-04 15:49:52 +0900

    Oval 면적을 구하는 프로그램 작성

--- MyApp.java
+++ MyApp.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315040 KimSeonMin, 2018315034 JeongCheolwoo, 2018315031 Parksunghyun)
+ * @version (2019/09/04)
  */
 public class MyApp
 
--- Oval.java
+++ Oval.java
@@ -10,15 +10,18 @@ public class Oval implements Shape
 {
     int x;
     int y;
-    
-    public void oval(int x, int y){
+
+    public Oval(int x, int y){
         this.x = x;
         this.y = y;
     }
+
+    public void draw(){
+        System.out.println(x + "x" + y + "에 내접하는 타원입니다.");
+    }
+
     public double getArea(){
         return x * y * PI;
     }
-    public void draw(){
-        System.out.println(x + "*" + y + "에 내접하는 타원입니다.");
-    }
+
 }
